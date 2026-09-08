ebedacf4ad195c958ed31bc3a0dd950099a3f36a
11249a183-dotcom <11249a183@kanchiuniv.ac.in>
2026-09-08 16:17:18 +0530

    Add files via upload

---
+++ FinalDemo.java
@@ -0,0 +1,7 @@
+class FinalDemo {
+    final int number = 10;
+    public static void main(String[] args) {
+        FinalDemo obj = new FinalDemo();
+        System.out.println("Final value: " + obj.number);
+    }
+}
